6a08dc58668a4b5c8b1e46b78caaa7175fc3ce28
gclarobatista <34254160+gclarobatista@users.noreply.github.com>
2020-05-31 23:05:22 +0100

    Updates

---
+++ precache-manifest.1b0a9dacfeca434e6a04e7ed2ad51a23.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd1166446dbcb3f1873837930d3c066",
+    "url": "/tp2-es-2020/index.html"
+  },
+  {
+    "revision": "c8b32f05a99a17d912fa",
+    "url": "/tp2-es-2020/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0ea6b6dcd783ced0bdaa",
+    "url": "/tp2-es-2020/static/css/main.23a9b7e3.chunk.css"
+  },
+  {
+    "revision": "c8b32f05a99a17d912fa",
+    "url": "/tp2-es-2020/static/js/2.a5a0eb27.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/tp2-es-2020/static/js/2.a5a0eb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea6b6dcd783ced0bdaa",
+    "url": "/tp2-es-2020/static/js/main.d45e45ae.chunk.js"
+  },
+  {
+    "revision": "009c7ba2da26ebe31a7b",
+    "url": "/tp2-es-2020/static/js/runtime-main.72b6fd4b.js"
+  },
+  {
+    "revision": "ad33e8325e0072dc96b5f0b8f864a2c3",
+    "url": "/tp2-es-2020/static/media/logo.ad33e832.png"
+  },
+  {
+    "revision": "bb6a439d77e91523c8e0f7a3f4bc5f0d",
+    "url": "/tp2-es-2020/static/media/reached.bb6a439d.png"
+  }
+]);
